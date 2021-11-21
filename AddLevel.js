3fe13146cd918c3c6adbe62673f8494c81bb0f80
pawelos231 <93586648+pawelos231@users.noreply.github.com>
2021-11-21 21:58:59 +0100

    Delete AddLevel.js

--- AddLevel.js
+++
@@ -1,9 +0,0 @@
-let mainDiv = document.querySelector(".LevelSelectContainer")
-let count = 5;
-document.querySelector("button").addEventListener("click", () => {
-    count++;
-    let div = document.createElement("div");
-    div.textContent = count
-    div.className = "level"
-    mainDiv.appendChild(div)
-})
